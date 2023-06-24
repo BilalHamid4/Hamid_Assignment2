2e4a975e33910951f9043deab400d4df25a46dda
Bhamid4 <113592135+Bhamid4@users.noreply.github.com>
2023-06-24 18:46:40 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+
+module.exports = 
+{
+    
+    //"URI": "mongodb://localhost/game_store"
+  "URI": "mongodb+srv://BilalHamid:B1234567890l@mongodbserver.opav6jv.mongodb.net/Contact_business_List?retryWrites=true&w=majority"
+}
